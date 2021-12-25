fc90886ba8b1628bd2915aa5be4a30d5ea6f87e4
Poyopote <94369664+Poyopote@users.noreply.github.com>
2021-12-26 00:43:18 +0100

    Create initialisation.php

---
+++ Portfolio/bdd/initialisation.php
@@ -0,0 +1,23 @@
+<?php
+	session_start();
+
+	if (empty($_SESSION['parle'])){ //si la session n'a pas de valeur langue
+		if (empty($_GET["lang"])){ //on regarde si get lang est vide
+			$lang = "fr";			//alors on met en francais
+		}
+		else {						//sinon
+			$lang = $_GET["lang"]; 	// la langue = le paramettre get
+			$_SESSION['parle'] = $lang; 	// et la session mémorise la langue
+		}
+	}
+	else {							// mais si la session a une valeur langue
+		if (empty($_GET["lang"])){ // si le get langue est vide
+			$lang = $_SESSION['parle'];			// la langue prendre prend la langue memoriser
+		}
+		else {						// sinon
+			$lang = $_GET["lang"]; 	// la langue = le paramettre get
+			$_SESSION['parle'] = $lang; 	// et la session mémorise encore la langue
+		}
+	}
+	
+?>
