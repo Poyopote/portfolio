a32a397b2c22972b991844c08c80125299b22f6b
Poyopote <94369664+Poyopote@users.noreply.github.com>
2021-12-26 00:43:04 +0100

    Create header.php

---
+++ Portfolio/bdd/header.php
@@ -0,0 +1,14 @@
+<header id="header">
+	<nav>
+		<input type="image" src="../img/menu.png" alt="menu">
+		<!-- <div class="logo"></div> -->
+		<div class="nav-menu">
+			<!-- <h1>Accueil</h1> -->
+			<a href="presentation.php" class="nav-lien"><img src="../img/menu-presenation.png" alt="menu"></a>
+			<a href="competence-et-etude.php" class="nav-lien"><img src="../img/menu-competence.png" alt="menu"></a>
+			<a href="../index.php" class="nav-lien"><img src="../img/menu-home.png" alt="home"></a>
+			<a href="realisation.php" class="nav-lien"><img src="../img/menu-projet.png" alt="menu"></a>
+			<a href="contact.php" class="nav-lien"><img src="../img/menu-contact.png" alt="menu"></a>
+		</div>
+	</nav>	
+</header>
