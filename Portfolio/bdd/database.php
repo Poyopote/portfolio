ef886ede17ff4481f57903ab980b0453497f9ae9
Poyopote <94369664+Poyopote@users.noreply.github.com>
2021-12-26 00:42:52 +0100

    Update database.php

--- Portfolio/bdd/database.php
+++ Portfolio/bdd/database.php
@@ -19,9 +19,34 @@
 			throw new \PDOException($e->getMessage(), (int)$e->getCode());
 		}
 	}
+
+	// Page d'accueil du portfolio
 	
-	function BDD($pdo, $lang)
+	function home($pdo, $lang)
 	{
-		return $pdo->query("SELECT `$lang` FROM `langue`;")->fetchAll();
+		return $pdo->query("SELECT `$lang` FROM `accueil`;")->fetchAll();
 	}
+
+	// Footer pour chaque page
+
+	function footer($pdo, $lang)
+	{
+		return $pdo->query("SELECT `$lang` FROM `footer`;")->fetchAll();
+	}
+
+	// fonction pour recup les projets en fonction de la langue.
+
+	function projet()
+	{
+		return $pdo->query();
+	}
+
+	function projet_info($pdo, $lang)
+	{
+		return $pdo->query("SELECT `titre`, `outil`, `$lang`
+		FROM `projet`
+		INNER JOIN `realisation_descript`
+		ON projet.unique_id = realisation_descript.unique_id;")->fetchAll();
+	}
+
 ?>
