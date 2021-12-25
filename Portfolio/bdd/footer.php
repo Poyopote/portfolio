691f64149f9784f42a2338e28e9b68bec85c0012
Poyopote <94369664+Poyopote@users.noreply.github.com>
2021-12-26 00:42:56 +0100

    Create footer.php

---
+++ Portfolio/bdd/footer.php
@@ -0,0 +1,20 @@
+	<footer class="clear">
+		<div class="div-footer">
+			<details>
+				<summary><?php echo $footer[0][$lang];  ?></summary>
+				<ul class="tab-langue">
+					<li><a href="?lang=fr" class="item france"><?php echo $footer[1][$lang];  ?></a></li>
+					<li><a href="?lang=en" class="item uk"><?php echo $footer[2][$lang];  ?></a></li>
+				</ul>
+			</details>
+		</div>
+		<div class="div-footer">
+			<p><?php echo $footer[3][$lang];  ?></p>
+		</div>
+		<div class="div-footer">
+			<p>Lorem Ipsum</p>
+		</div>
+    <div class="div-footer">
+      <p>01.02.03.04.05</p>
+    </div>
+	</footer>
