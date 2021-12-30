96d16980eacec9bdcdf810dfbab4dab57e28662b
Poyopote <94369664+Poyopote@users.noreply.github.com>
2021-12-30 21:42:49 +0100

    Update menu-folio.js

--- Portfolio/script/menu-folio.js
+++ Portfolio/script/menu-folio.js
@@ -1,31 +1,17 @@
 function bouton_menu() {
 	let navigation = document.querySelector(".nav-menu") ;
 	let lien = document.querySelectorAll(".nav-menu a");
-	let ombre = document.querySelector("#header nav") ; //header#header nav div.nav-menu ::before
+	let ombre = document.getElementById('overlay'); 
 	console.log(navigation);
-	if (navigation.style.display === 'none') {
+
+	if (navigation.style.display === 'none' && ombre.style.display === 'none') {
 		navigation.style.display = 'block';
-		console.log(lien);
-		
-		function mouseOver() {
-			ombre.classList.add("sombre");
-		}
-
-		function mouseOut() {
-			ombre.classList.remove("sombre");
-		}
-
-		// let chrono = setInterval(onTick, 50);
-
-		for (let i = 0; i < lien.length; i++){
-			lien[i].addEventListener("mouseover", mouseOver);
-			lien[i].addEventListener("mouseout", mouseOut);
-		}
-		// ombre.classList.add("sombre");
+		ombre.style.display = 'block';
 
 	}
 		
 	else{
+		ombre.style.display = 'none';
 		navigation.style.display = 'none';
 	} 
 		
@@ -41,3 +27,20 @@ function bouton_menu() {
 		// 	console.log(ombre);
 		// }
 
+		//version 1 et 2
+
+		// function mouseOver() {
+		// 	ombre.classList.add("sombre");
+		// }
+
+		// function mouseOut() {
+		// 	ombre.classList.remove("sombre");
+		// }
+
+		// // let chrono = setInterval(onTick, 50);
+
+		// 		for (let i = 0; i < lien.length; i++){
+		// 			lien[i].addEventListener("mouseover", mouseOver);
+		// 			lien[i].addEventListener("mouseout", mouseOut);
+		// 		}
+		// 		// ombre.classList.add("sombre");
