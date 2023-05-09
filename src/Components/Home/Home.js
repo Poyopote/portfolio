1d7c30dbb92e6ada17c9cb640b30f1d144d5f6a7
Poyopote <94369664+Poyopote@users.noreply.github.com>
2023-05-09 19:00:04 +0200

    Home Anima

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,15 +3,16 @@ import './Home.css';
 import logo from '../../assets/img/logo.svg';
 import pdf from '../../assets/doc/CV 2023 -04.pdf';
 import Animation from "./animation/Anima";
+import Typewriter from "./animation/Typewriter";
 
 
 function Home() {
     return (
-        <header id="main-header" className="relative flex flex-col items-center justify-center">
-        <Animation className="absolute z-0"/>
-        <div className="logo-container flex-initial z-10">
-          <img src={logo} alt="Logo SL" className="logo inline-block" />
-          <h1 className="site-title">My Portfolio</h1>
+        <header id="main-header" className="relative flex flex-col items-center justify-center md:h-screen">
+        <Animation className="absolute z-0 "/>
+        <div className="logo-container z-10">
+          <img src={logo} alt="Logo SL" className="logo inline-block  max-w-[280px]"/>
+          <Typewriter />
         </div>
         <div className="buttons ">
           <a href={pdf} className="btn1">
--- src/Components/Home/animation/Anima.js
+++ src/Components/Home/animation/Anima.js
@@ -8,9 +8,9 @@ function Animation() {
     
         <div className="waves-container">
     
-          <div className="wave wave5"></div>
-          <div className="wave wave4"></div>
-          <div className="wave wave3"></div>
+          <div className="wave wave5 opacity-70 md:opacity-100"></div>
+          <div className="wave wave4 opacity-30 md:opacity-100"></div>
+          <div className="wave wave3 opacity-30 md:opacity-100"></div>
           <div className="wave wave2"></div>
           <div className="wave wave1"></div>
 
