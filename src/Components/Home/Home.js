beb224983887e42c07ae15e04e365ef26c740e7b
Poyopote <94369664+Poyopote@users.noreply.github.com>
2023-05-11 13:32:20 +0200

    add Button

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,30 +4,35 @@ import logo from '../../assets/img/logo.svg';
 import pdf from '../../assets/doc/CV 2023 -04.pdf';
 import Animation from "./animation/Anima";
 import Typewriter from "./animation/Typewriter";
+import Button from "./animation/Button";
+import { DocumentTextIcon } from '@heroicons/react/24/solid';
+import { EnvelopeOpenIcon } from '@heroicons/react/24/solid';
+
 
 
 function Home() {
     return (
         <header id="main-header" className="relative flex flex-col items-center justify-center md:h-screen">
-        <Animation className="absolute z-0 "/>
+        <Animation className=""/>
         <div className="logo-container z-10">
-          <img src={logo} alt="Logo SL" className="logo inline-block  max-w-[280px]"/>
-          <Typewriter />
+          <img src={logo} alt="Logo SL" loading="lazy" className="logo inline-block w-52 md:w-[280px] p-4"/>
+          <Typewriter className=" text-4xl sm:text-6xl h-6 relative"/>
         </div>
-        <div className="buttons ">
-          <a href={pdf} className="btn1">
-            
-            Mon CV
-          </a>
-          <a href="contact.html" className="btn2">
-            Contact
-          </a>
+        <div className="flex z-10 w-full justify-center ">
+        
+          <Button color="primaire" link={pdf} text="Mon CV">
+            <DocumentTextIcon className="h-6 w-6" />
+          </Button>
+          <Button color="secondaire" link="contact.html" text="Contact">
+            <EnvelopeOpenIcon className="h-6 w-6" />
+          </Button>
         </div>
+        
         <nav className="main-nav z-10">
           <ul className="nav-list flex justify-center space-x-4">
             <li>
               <a href="#main-header" className="nav-link active">
-                Home
+                Home 
               </a>
             </li>
             <li>
