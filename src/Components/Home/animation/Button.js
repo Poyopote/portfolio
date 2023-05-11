012a17c4839192a20c56c613d146167766221f53
Poyopote <94369664+Poyopote@users.noreply.github.com>
2023-05-11 13:31:39 +0200

    Create Button.js

---
+++ src/Components/Home/animation/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Children } from 'react';
+
+const Button = props => {
+  return (
+    <a href={props.link} className={`button-folio m-2 relative button-${props.color}`}>
+      <span className="pulse"></span>
+      <span className='flex'>{props.children}&nbsp;{props.text}</span>
+    </a>
+  );
+};
+
+export default Button;
