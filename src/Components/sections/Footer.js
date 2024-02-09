6ffb911a491c440e75214b710f8c4cac395c2715
Poyopote <94369664+Poyopote@users.noreply.github.com>
2024-02-09 17:01:34 +0100

    Update Footer.js

--- src/Components/sections/Footer.js
+++ src/Components/sections/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Footer()  {
     return (
-        <footer>
-            <div className='text-center py-10 bg-customLightGray text-customBlack'>
+        <footer className="hidden md:block">
+            <div className='text-center py-10 text-customBlack'>
                 Conçu et réalisé par Steven LADOUR
             </div>
         </footer>
